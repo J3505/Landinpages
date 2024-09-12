11c5d954e01eed0ea2188f6ae5d7180c2e0f58c0
Jesus <alonstarkj@gmail.com>
2024-09-12 18:07:06 -0500

    hh

---
+++ vite-project/src/components/cards/Card3.jsx
@@ -0,0 +1,10 @@
+import { SlArrowRightCircle } from "react-icons/sl";
+
+function Card3 (){
+    return(
+        <>
+            <SlArrowRightCircle />
+        </>
+    );
+}
+export default Card3
--- vite-project/src/components/content/Content1.jsx
+++ vite-project/src/components/content/Content1.jsx
@@ -2,9 +2,80 @@
 const Content1 = () => {
   return (
     <>
-    <div className="container-content1">
-      hola
-    </div>
+      <div className="container-content1">
+        {/*  */}
+
+
+        <div className="texto-1">
+          <p>Lorem ipsum</p>
+        </div>
+
+        <div className="texto-2">
+
+          <div className="textoA">
+            {/* <!-- <p>Explro the Wordl with us </p> --> */}
+            <div className="palabra">
+              Explore the
+            </div>
+            <div className="palabra">
+              Wordl with us
+            </div>
+          </div>
+
+          <div className="textoB">
+            <div className="botn">
+              <div className="textoLoren">Lorean more</div>
+              <div className="espacio"></div>
+              <div className="flechadiagonal">
+                {/* <img src="img/flechaDiagonal.svg" alt="aun"> */}
+              </div>
+            </div>
+          </div>
+
+        </div>
+
+      
+      <div className="capa1-hijo2">
+
+        <div className="circulos">
+
+          <div className="capa-2">
+            <div className="flecha">
+              <p>contact</p>
+            </div>
+
+          </div>
+
+          <div className="capa-3">
+            <div className="flecha">
+              {/* <img src="img/flechaAbajo.svg" alt="aun"> */}
+            </div>
+
+          </div>
+
+
+        </div>
+
+        <div className="card">
+          <div className="circulo">
+            <div className="icono">
+              {/* <img src="./img/fleDerecha.svg" alt=""> */}
+            </div>
+          </div>
+          <div className="parrafo">
+            <p>Explore our curated list of must-vist destinations around the globe.</p>
+          </div>
+          <div className="linea"></div>
+          <div className="palabras-tres">
+            <div className="letra1"><span>(FIRST)</span></div>
+            <div className="letra2"><span>(PRESENT)</span></div>
+            <div className="letra3"><span>01</span></div>
+          </div>
+        </div>
+      </div>
+
+
+    </div >
       
     </>
   )
